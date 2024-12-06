e562e960d1e2963e4b2f7870506fb55ff3cef2cb
Chris Vaillancourt <cvaillan22@gmail.com>
2024-12-06 15:08:05 -0800

    feat: add cart

---
+++ padre-ginos/src/Cart.jsx
@@ -0,0 +1,28 @@
+const intl = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+export default function Cart({ cart, checkout }) {
+  const total = cart.reduce((total, curr) => {
+    total += curr.pizza.sizes[curr.size];
+    return total;
+  }, 0);
+
+  return (
+    <div className="cart">
+      <h2>Cart</h2>
+      <ul>
+        {cart.map((item, index) => (
+          // using index as key because there can be duplicates in the cart
+          <li key={index}>
+            <span className="size">{item.size}</span> -
+            <span className="type">{item.pizza.name}</span> -
+            <span className="price">{item.price}</span> -
+          </li>
+        ))}
+      </ul>
+      <p>Total: {intl.format(total)}</p>
+      <button onClick={checkout}>Checkout</button>
+    </div>
+  );
+}
--- padre-ginos/src/Order.jsx
+++ padre-ginos/src/Order.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Pizza from "./Pizza";
+import Cart from "./Cart";
 
 const intl = new Intl.NumberFormat("en-US", {
   style: "currency",
@@ -11,6 +12,24 @@ export default function Order() {
   const [loading, setLoading] = useState(true);
   const [pizzaType, setPizzaType] = useState("pepperoni");
   const [pizzaSize, setPizzaSize] = useState("M");
+  const [cart, setCart] = useState([]);
+
+  async function checkout() {
+    setLoading(true);
+
+    await fetch("/api/order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        cart,
+      }),
+    });
+
+    setCart([]);
+    setLoading(false);
+  }
 
   // don't need to be a hook because it's a derived (computed) state
   let price, selectedPizza;
@@ -32,67 +51,79 @@ export default function Order() {
   }, []);
 
   return (
-    <div className="order">
-      <h2>Create Order</h2>
-      <form>
-        <div>
+    <div className="order-page">
+      <div className="order">
+        <h2>Create Order</h2>
+        {/* it's better for us to put the event listener on the form
+      instead of the button inside of the form so someone can hit 
+      enter without needing to click the submit button. It's also better
+      for accessibility. */}
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setCart([
+              ...cart,
+              { pizza: selectedPizza, size: pizzaSize, price },
+            ]);
+          }}
+        >
           <div>
-            <label htmlFor="pizza-type">Pizza Type</label>
-            <select
-              onChange={(e) => setPizzaType(e.target.value)}
-              name="pizza-type"
-              value={pizzaType}
-            >
-              {pizzaTypes.map((pizza) => (
-                <option key={pizza.id} value={pizza.id}>
-                  {pizza.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label htmlFor="pizza-size">Pizza Size</label>
             <div>
-              <span>
-                <input
-                  checked={pizzaSize === "S"}
-                  onChange={(e) => setPizzaSize(e.target.value)}
-                  type="radio"
-                  name="pizza-size"
-                  value="S"
-                  id="pizza-s"
-                />
-                <label htmlFor="pizza-s">Small</label>
-              </span>
-              <span>
-                <input
-                  checked={pizzaSize === "M"}
-                  onChange={(e) => setPizzaSize(e.target.value)}
-                  type="radio"
-                  name="pizza-size"
-                  value="M"
-                  id="pizza-m"
-                />
-                <label htmlFor="pizza-m">Medium</label>
-              </span>
-              <span>
-                <input
-                  checked={pizzaSize === "L"}
-                  onChange={(e) => setPizzaSize(e.target.value)}
-                  type="radio"
-                  name="pizza-size"
-                  value="L"
-                  id="pizza-l"
-                />
-                <label htmlFor="pizza-l">Large</label>
-              </span>
+              <label htmlFor="pizza-type">Pizza Type</label>
+              <select
+                onChange={(e) => setPizzaType(e.target.value)}
+                name="pizza-type"
+                value={pizzaType}
+              >
+                {pizzaTypes.map((pizza) => (
+                  <option key={pizza.id} value={pizza.id}>
+                    {pizza.name}
+                  </option>
+                ))}
+              </select>
             </div>
+            <div>
+              <label htmlFor="pizza-size">Pizza Type</label>
+              <div>
+                <span>
+                  <input
+                    onChange={(e) => setPizzaSize(e.target.value)}
+                    checked={pizzaSize === "S"}
+                    type="radio"
+                    name="pizza-size"
+                    value="S"
+                    id="pizza-s"
+                  />
+                  <label htmlFor="pizza-s">Small</label>
+                </span>
+                <span>
+                  <input
+                    onChange={(e) => setPizzaSize(e.target.value)}
+                    checked={pizzaSize === "M"}
+                    type="radio"
+                    name="pizza-size"
+                    value="M"
+                    id="pizza-m"
+                  />
+                  <label htmlFor="pizza-m">Medium</label>
+                </span>
+                <span>
+                  <input
+                    onChange={(e) => setPizzaSize(e.target.value)}
+                    checked={pizzaSize === "L"}
+                    type="radio"
+                    name="pizza-size"
+                    value="L"
+                    id="pizza-l"
+                  />
+                  <label htmlFor="pizza-l">Large</label>
+                </span>
+              </div>
+            </div>
+            <button type="submit">Add to Cart</button>
           </div>
-          <button type="submit">Add to Cart</button>
-        </div>
-        <div className="order-pizza">
           {loading ? (
-            <h3>Loading...</h3>
+            <h3>LOADING …</h3>
           ) : (
             <div className="order-pizza">
               <Pizza
@@ -103,8 +134,9 @@ export default function Order() {
               <p>{price}</p>
             </div>
           )}
-        </div>
-      </form>
+        </form>
+      </div>
+      {loading ? <h2>LOADING …</h2> : <Cart checkout={checkout} cart={cart} />}
     </div>
   );
 }
